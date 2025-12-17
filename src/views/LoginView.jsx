bf9d7ac4a306eac3f08a67c574bbffc12e776b25
colddcereal <jirapatthepsurin@192.168.1.3>
2025-12-17 12:59:07 +0700

    add login & singup view

---
+++ src/views/LoginView.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function LoginView() {
+  return (
+    <div className='flex flex-row border border-amber-400'>
+        <img src="https://i.pinimg.com/736x/73/d3/fb/73d3fb41823716f69cc59695e6a91e81.jpg" alt=""  className='hidden md:block w-1/2'/>
+        <div className='flex flex-col  border border-amber-500 items-center w-full'>
+        <h1>VELVE</h1>
+        <h2>Welcome back</h2>
+        <p>what</p>
+        </div>
+    </div>
+  )
+}
+
+export default LoginView
---
+++ src/views/SingupView.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SingupView = () => {
+  return (
+    <div>SingupView</div>
+  )
+}
